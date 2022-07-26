e25396d9eec19641cfe6f895e0144002467a254f
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-07-26 15:23:34 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const Row: React.FC<{ todo: Todo }> = ({ todo }) => {
+  return (
+    <>
+      <h4>{todo.title}</h4>
+      <h5>
+        Completed?
+        {todo.completed ? "✔️" : "❌"}
+      </h5>
+    </>
+  );
+};
 
 function App() {
+  const [json, setJson] = useState<Todo[]>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/"
+      );
+      const data = (await response.json()) as Todo[];
+      setJson(data);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{json && json.map((todo) => <Row todo={todo} />)}</div>
   );
 }
 
